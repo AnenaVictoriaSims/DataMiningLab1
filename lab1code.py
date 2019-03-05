4ad3dfbcc0aea1cbc77a0e7b3aa6167e441fc125
AnenaVictoriaSims <anena.sims@mavs.uta.edu>
2019-03-04 19:24:56 -0600

    Updated code that generates random data

--- lab1code.py
+++ lab1code.py
@@ -6,22 +6,27 @@ import matplotlib.pyplot as plt
 
 #Problem 1: k-means
 def problemOne(mean1, mean2, cov):
-	x1, y1 = genGauss2D(mean1, cov)						#assign the generated sets to variables
-	x2, y2 = genGauss2D(mean2, cov)
+	x, y, set1 = genGauss2D(mean1, cov)						#Assign the generated sets and coords to variables
+	a, b, set2 = genGauss2D(mean2, cov)
 
-	printGraphData(x1, y1, x2, y2)						#print the graph data
+	printGraphData(x, y, a, b)								#Graph the data points using coords
+	print("Set 1: ")										#Print the arrays of random data to check work
+	print(set1)
+	print("Set 2: ")
+	print(set2)
 
-#Randomly generate 2 sets of data containing 500 samples using given parameters	
+#Randomly generate the sets of data containing 500 samples using given parameters	
 def genGauss2D(mean, cov):										
-	x, y = np.random.multivariate_normal(mean, cov, 500).T
-	return(x, y)
+	sampleData = np.random.multivariate_normal(mean, cov, 500)		#Get 2D array of random data (2col, 500row)
+	x, y = sampleData.T 											#Get the x & y coords for each data point to plot
+	return(x, y, sampleData)
 
 #Cluster the data 
 
-#Print graph data for problem 1
-def printGraphData(x1, y1, x2, y2):								
-	plt.plot(x1, y1, '*')							# This is displayed in blue
-	plt.plot(x2, y2, '*')							# This is displayed in orange
+#Graph the coordinates of the data points for both sets
+def printGraphData(x, y, a, b):								
+	plt.plot(x, y, '*')							#Set1 is displayed in blue
+	plt.plot(a, b, '*')							#Set2 is displayed in orange
 	plt.axis('equal')
 	plt.show()
 
